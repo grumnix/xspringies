e0ad0c736efde6aa97b7a0ca2d9bbda62f53796c
Ingo Ruhnke <grumbel@gmail.com>
2022-08-28 23:18:23 +0200

    Initial checkin

---
+++ flake.nix
@@ -0,0 +1,55 @@
+ {
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        packages = rec {
+          default = xspringies;
+
+          xspringies = pkgs.stdenv.mkDerivation rec {
+            pname = "xspringies";
+            version = "1.12";
+
+            src = ./xspringies-1.12.tar.Z;
+
+            postPatch = ''
+              rm -v Imakefile
+              cp -v Makefile.std Makefile
+              substituteInPlace Makefile \
+                --replace "/bin/cp" \
+                          "cp" \
+               --replace "DDIR = /usr/games/" \
+                          "DDIR = $out/" \
+                --replace "CFLAGS = " \
+                          "CFLAGS = -Wno-error=format-security "
+            '';
+
+            buildPhase = ''
+              make
+            '';
+
+            installPhase = ''
+              mkdir -p $out/bin
+              make install
+            '';
+
+            nativeBuildInputs = with pkgs; [
+              xorg.imake
+              xorg.gccmakedep
+            ];
+
+            buildInputs = with pkgs; [
+              xorg.libX11
+            ];
+          };
+        };
+      }
+    );
+}
+
